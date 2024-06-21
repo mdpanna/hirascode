2da61a33f1f66a224bfff087b73a32eff844aa75
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2024-06-21 12:43:12 -0400

    chore: Add .env to .gitignore and update npm dependencies

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Home from './component/Home';
+import P from './Optimizeway/P';
 function App() {
   return (
    <>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/p" element={<P/>} />
         </Routes>
       </Router>
    </>
